c62dd11637b62a87be2f7d82068188d5c380c598
sebashtioon <sebastiansuciu607@gmail.com>
2025-09-04 21:04:00 +1000

    Add global player state and fade transition logic
    Introduces a global autoload singleton to manage player activity state. Updates main_level.gd and main_level.tscn to include fade-in/fade-out transitions on play, using a new FadeManager AnimationPlayer. Refactors 3DPlayer_SCRIPT.gd to respect the global player_active state, enabling/disabling player movement and input accordingly.

--- src/main_level/main_level.gd
+++ src/main_level/main_level.gd
@@ -1,9 +1,17 @@
 extends Node3D
 
 func _ready() -> void:
+	global.player_active = false
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	$Camera3D/MenuAnimation.play("main")
 
 
 func _on_play_button_pressed() -> void:
 	$Camera3D/MenuAnimation.play("main", -1, -2, true)
+	
+	
+	# Fade in and out, + camera switch
+	await get_tree().create_timer(1.0).timeout
+	$Camera3D/FadeManager.play("fade")
+	await get_tree().create_timer(3.0).timeout
+	$Camera3D/FadeManager.play("fade", -1, -0.35, true)
