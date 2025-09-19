896505f47ccc3a4ef92f4a4d18d5c7c70aa2bde9
sebashtioon <sebastiansuciu607@gmail.com>
2025-09-19 17:47:03 +1000

    Add sackcloth bed to main level and update interaction
    Integrated the sackcloth bed scene into the main level and adjusted its interaction logic. The sleep action now triggers the player's sleep method if conditions are met, and the quick message was updated for clarity. Scene resource paths were also corrected for proper UI display.

--- ast/models/items/sackcloth bed/sackcloth_bed.gd
+++ ast/models/items/sackcloth bed/sackcloth_bed.gd
@@ -3,6 +3,6 @@ extends Node3D
 
 func _on_player_sleep() -> void:
 	if global.fire_lighted:
-		pass
+		global.player.sleep() # TODO
 	else:
-		global.player.display_quick_message("Not enough wood planks!")
+		global.player.display_quick_message("You must complete your tasks first!")
